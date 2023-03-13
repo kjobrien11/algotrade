31cabd10a548df53f4f64c9666ae2b0bded43912
KJ O'BRIEN <kjokjo219@gmail.com>
2023-03-13 12:33:25 +0000

    Creates index.js and some basic code for alpaca

---
+++ index.js
@@ -0,0 +1,10 @@
+const Alpaca = require("@alpacahq/alpaca-trade-api");
+
+const options = {
+    keyId: "<Your API Key>",
+    secretKey: "<Your Secret Key>",
+    paper: true,
+  };
+
+  
+console.log('hello');
