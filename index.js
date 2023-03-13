58803b2888db65df4bab9f6ca1e4457ed351ad4e
KJ O'BRIEN <kjokjo219@gmail.com>
2023-03-13 13:00:03 +0000

    Working model to paper trade inputted values

--- index.js
+++ index.js
@@ -7,5 +7,31 @@ const options = {
     paper: true,
   };
 
+//create alpaca object
+const alpaca = new Alpaca(options);
+
+//account info
+// alpaca.getAccount().then((account) => {
+//     console.log("Current Account:", account);
+//   });
+
+const buyParams = {
+    symbol: "AAPL",
+    qty: 1,
+    side: "buy",
+    type: "market",
+    time_in_force: "gtc",
+  };
+  
+Buy a stock
+  alpaca.createOrder(buyParams).then((order) => {
+    console.log("Order details: ", order);
+  });
+
+//show positions
+  alpaca.getPositions().then((positions) => {
+    positions.forEach((position) => console.log(position));
+  });
+
 
 console.log('working per usual');
