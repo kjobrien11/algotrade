5ef7ec27ced5464660db97673621598e3009b82a
KJ O'BRIEN <kjokjo219@gmail.com>
2023-03-13 12:41:47 +0000

    Configures program to run with environment variables for api key information

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const Alpaca = require("@alpacahq/alpaca-trade-api");
+require('dotenv').config()
 
 const options = {
-    keyId: "<Your API Key>",
-    secretKey: "<Your Secret Key>",
+    keyId: process.env.API_KEY,
+    secretKey: process.env.SECRET_KEY,
     paper: true,
   };
 
-  
-console.log('hello');
+
+console.log('working per usual');
